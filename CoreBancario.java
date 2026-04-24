c1f8a98b38bc34b8ff0b870958ba4f38cfa4e45a
Mariale27 <m_leal7@unisimon.edu.co>
2026-04-24 11:38:37 -0500

    se agregó el archivo corebancario

---
+++ CoreBancario.java
@@ -0,0 +1,39 @@
+import java.util.*; 
+ 
+/** 
+ * Sistema de Gestión de Transacciones TechPay 
+ * Versión 1.0 - Módulo de Pruebas 
+ */ 
+public class CoreBancario { 
+    private double balance; 
+    private List<String> historial; // DEFECTO DATOS: Lista no inicializada 
+ 
+    // MÓDULO DE TRANSACCIONES 
+    public void ejecutarTransferencia(double monto, String destino) { 
+        // DEFECTO LÓGICO: No verifica si el balance es suficiente para el monto 
+        // DEFECTO INTERFAZ: No valida si el destino es nulo o vacío 
+        balance -= monto; 
+        historial.add("Transferencia a " + destino + ": " + monto); 
+    } 
+ 
+    // MÓDULO DE SEGURIDAD 
+    public boolean validarAcceso(String pin) { 
+        // DEFECTO LÓGICO: Validación débil (solo verifica longitud de 4 dígitos) 
+        if (pin.length() == 4) { 
+            return true; 
+        } 
+        return false; 
+    } 
+ 
+     /*Aplica un interés del 1% anual sobre el saldo actual. 
+     * DEFECTO DOCUMENTACIÓN: El comentario indica 1%, pero el código aplica 10% 
+     */ 
+    public void aplicarInteresAnual() { 
+        balance = balance + (balance * 0.1);  
+    } 
+ 
+    public void imprimirEstado() { 
+        // DEFECTO SALIDA: Mensaje de depuración poco profesional 
+        System.out.println("Saldo: " + balance); 
+    } 
+}
